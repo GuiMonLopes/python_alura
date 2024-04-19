871651207c0cadc4e53136c1fa04bc679c6367b1
Guilherme <guilherme_monteiro1@hotmail.com>
2024-04-19 15:04:07 -0300

    Estudos de Herança e Polimorfismo

--- python_OO/app.py
+++ python_OO/app.py
@@ -1,13 +1,19 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.bebida import Bebida
 
-restaurante_praca = Restaurante('praça','Gourmet')
-restaurante_praca.receber_avaliacao('Guilherme', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emyli', 5)
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+bebida_suco = Bebida('Suco de manga', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
 
 
 def main():
-    Restaurante.listar_restaurentes()
+    restaurante_praca.exibir_cardapio
 
 
 if __name__ == '__main__':
